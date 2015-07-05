cf7b442fb436ad032a60372bbd90705f98357325
Chris Butler <chris31389@gmail.com>
2015-07-05 14:09:50 +0100

    trying basic call only

--- bin/install.js
+++ bin/install.js
@@ -1,10 +1,4 @@
 #! /usr/bin/env node
+
 var shell = require("shelljs");
-shell.exec("echo shell.exec works");
-var envVar = process.env.OPENSHIFT_REPO_DIR;
-shell.exec("echo OPENSHIFT_REPO_DIR  = " + envVar);
-if (envVar != undefined) {
-  shell.exec("HOME=$OPENSHIFT_REPO_DIR bower install");
-}else{
-  shell.exec("bower install");
-}
+shell.exec("HOME=$OPENSHIFT_REPO_DIR bower install");
