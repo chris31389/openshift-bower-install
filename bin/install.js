0e1cd0585fd9f349451171897af5aa1454112e6c
Chris Butler <chris31389@gmail.com>
2015-07-05 14:02:29 +0100

    trying REPO

--- bin/install.js
+++ bin/install.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 shell.exec("echo shell.exec works");
-var envVar = process.env.OPENSHIFT_DATA_DIR;
-shell.exec("echo OPENSHIFT_DATA_DIR = " + envVar);
+var envVar = process.env.OPENSHIFT_REPO_DIR;
+shell.exec("echo OPENSHIFT_REPO_DIR  = " + envVar);
 if (envVar != undefined) {
-  shell.exec("HOME=$OPENSHIFT_DATA_DIR bower install");
+  shell.exec("HOME=$OPENSHIFT_REPO_DIR  bower install");
 }else{
   shell.exec("bower install");
 }
