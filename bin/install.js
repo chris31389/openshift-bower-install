21b7bf7a80c3b6047c188e17dd1472bd89b0cb34
Chris Butler <chris31389@gmail.com>
2015-07-05 14:04:34 +0100

    bumping version

--- bin/install.js
+++ bin/install.js
@@ -4,7 +4,7 @@ shell.exec("echo shell.exec works");
 var envVar = process.env.OPENSHIFT_REPO_DIR;
 shell.exec("echo OPENSHIFT_REPO_DIR  = " + envVar);
 if (envVar != undefined) {
-  shell.exec("HOME=$OPENSHIFT_REPO_DIR  bower install");
+  shell.exec("HOME=$OPENSHIFT_REPO_DIR bower install");
 }else{
   shell.exec("bower install");
 }
