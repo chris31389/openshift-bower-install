bfe12d6d1ece632711d7b9663b07e43d48080ed6
Chris Butler <chris31389@gmail.com>
2015-07-05 14:15:19 +0100

    building up

--- bin/install.js
+++ bin/install.js
@@ -1,4 +1,6 @@
 #! /usr/bin/env node
 
 var shell = require("shelljs");
+var envVar = process.env.OPENSHIFT_REPO_DIR;
+shell.exec("echo RepoEnv: " + envVar);
 shell.exec("HOME=$OPENSHIFT_REPO_DIR bower install");
