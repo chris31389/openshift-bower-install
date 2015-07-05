dd4f306f0dcd5f4b776ad74f210ecd3984c28261
Chris Butler <chris31389@gmail.com>
2015-07-05 13:30:21 +0100

    changing OPENSHIFT_ENV_VAR to OPENSHIFT_DATA_DIR

--- bin/install.js
+++ bin/install.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 shell.exec("echo shell.exec works");
-var envVar = process.env.OPENSHIFT_ENV_VAR;
-shell.exec("echo OPENSHIFT_ENV_VAR = " + envVar);
+var envVar = process.env.OPENSHIFT_DATA_DIR;
+shell.exec("echo OPENSHIFT_DATA_DIR = " + envVar);
 if (envVar != undefined) {
-  shell.exec("HOME=" + envVar);
+  shell.exec("HOME=OPENSHIFT_DATA_DIR");
   shell.exec("echo HOME=" + envVar);
 }
 shell.exec("bower install");
